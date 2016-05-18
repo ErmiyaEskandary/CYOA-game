3873f8ceef197ca8d24bff7b2d119fe98941bc38
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-18 17:57:57 +0100

    Wrong method of commenting out code
    Ahhh.... Javascript..

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-/*
+'''
 Copyright (C) 2016 Ermiya Eskandary
 This program is free software: 
 you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -8,4 +8,4 @@ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program.
 If not, see <http://www.gnu.org/licenses/>./gpl-howto.html
-*/
+'''
